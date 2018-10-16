a0018769e4d0e13f28278064668fb3a83eb4a353
Ruben <RUBEN@MXROBOTS.COM>
2018-10-16 16:46:54 +0200

    Add and correct exercise 1

---
+++ Lab02.R
@@ -0,0 +1,17 @@
+sheldon_game <- function(player1 , player2) {
+  alt <- c("rock", "lizard", "spock", "scissors", "paper")
+  stopifnot(player1 %in% alt, player2 %in% alt)
+  alt1 <- which(alt %in% player1)
+  alt2 <- which(alt %in% player2)
+  if (alt1 == alt2) {
+    return("Draw!")
+  }
+  else{
+    if (any((alt1 + c(1, 3)) %% 5 == alt2)) {
+      return("Player 1 wins!")
+    }
+    else {
+      return("Player 2 wins!")
+    }
+  }
+}
