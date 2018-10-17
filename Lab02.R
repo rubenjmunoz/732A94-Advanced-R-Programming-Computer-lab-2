c113aa69c8e5a2a016752fd38a09fbec9db7cfa1
Ruben <RUBEN@MXROBOTS.COM>
2018-10-17 09:01:35 +0200

    Finish exercise 9

--- Lab02.R
+++ Lab02.R
@@ -19,3 +19,83 @@ sheldon_game <- function(player1 , player2) {
 my_moving_median <- function(x , n, ...){
   return(unlist(lapply(1:(length(x) - n), function(y) median(x[y:(y + n)], na.rm = ...))))
 }
+
+for_mult_table <- function(from, to){
+  if(!is.numeric(c(from,to))){
+    stop()
+  }
+  return(c(from:to) %o% c(from:to))
+}
+
+cor_matrix <- function(df){
+  return(cor(df))
+}
+
+find_cumsum <- function(x, find_sum) {
+  if(!is.numeric(c(x, find_sum))){
+    stop()
+  }
+  s <- 0
+  i <- 0
+  while (s <= find_sum) {
+    i = i + 1
+    if (!is.na(x[i])) {
+      s <- s + x[i]
+    }
+    else{
+      s <- s + 0
+    }
+    if(i == length(x)){
+      break()
+    }
+  }
+  return(s)
+}
+
+
+while_mult_table <- function(from, to){
+  return(c(from:to) %o% c(from:to))
+}
+
+trial_division_factorization <- function(x) {
+  a <- c()             
+  f <- 2               
+  while (x > 1) {
+    if (x %% f == 0) {
+      a <- c(a, f)        
+      x <- x / f      
+    }
+    else{
+      f = f + 1       
+    }
+  }
+  return (a)
+}
+
+repeat_find_cumsum <- function(x, find_sum) {
+  i <- 0
+  s <- 0
+  repeat {
+    i = i + 1
+    s = s + x[i]
+    if ((i == length(x)) ||(s > find_sum)) {
+      break()
+    }
+  }
+  return(s)
+}
+
+repeat_my_moving_median <- function(x, n, ...){
+  i <- 0
+  v <- c()
+  repeat{
+    i = i + 1
+    v = c(v, median(x[i:(n + i)], na.rm = ...))
+    if ((length(x) - i) == n){
+      break()
+    }
+  }
+  return(v)
+}
+# print(repeat_my_moving_median(x = 5:15, n=4))
+# print(repeat_my_moving_median(x = c(5,1,2,NA,2,5,6,8,9,9), n=2))
